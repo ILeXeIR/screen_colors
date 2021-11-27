8c3ae1f01c2d5d37d1a8cc38554199672f9d0e1d
Alexander <alexander@example.com>
2021-11-27 16:29:49 +0300

    fullscreen and instruction

--- colors.py
+++ colors.py
@@ -1,12 +1,36 @@
 import sys
 import pygame
 
+class Instructions():
+	#Класс для отображения инструкций по управлению на экране
+
+	def __init__(self, colors):
+		self.screen = colors.screen
+		self.screen_rect = colors.screen.get_rect()
+		self.message = "Instructions"
+		self.font = pygame.font.Font(None, 30)
+		self.text_color = (230, 230, 230)
+		self.text = self.font.render(self.message, 1, self.text_color)
+		self.rect = self.text.get_rect()
+		self.rect.topleft = (100, 50)
+		
+	def blitme(self):
+		self.screen.blit(self.text, self.rect)
+
+	def update(self):
+		x = colors.red + colors.green + colors.blue
+		if x > 350:
+			self.text_color = (30, 30, 30)
+		else:
+			self.text_color = (230, 230, 230)
+		self.text = self.font.render(self.message, 1, self.text_color)
+
 class ColoredScreen():
 	#Класс для получения окна с возможной регулировкой цвета
 
 	def __init__(self):
 		pygame.init()
-		self.screen = pygame.display.set_mode((1200, 800))
+		self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 		self.screen_rect = self.screen.get_rect()
 		pygame.display.set_caption('Colors')
 
@@ -19,28 +43,33 @@ class ColoredScreen():
 		self.red_less_flag = False
 		self.green_less_flag = False
 		self.blue_less_flag = False
+		self.i_flag = True
+
+		self.instruct = Instructions(self)
 
 	def run_program(self):
 		#Запуск основной программы
 		while True:
 			self._check_events()
 			self._update_color()
+			self.instruct.update()
 			self._update_screen()
 
 	def _update_color(self):
 		#Обновляет цвет при нажатии соответствующих клавиш
+		speed = 0.5
 		if self.red_more_flag and self.red < 255:
-			self.red += 1
+			self.red += speed
 		if self.red_less_flag and self.red > 0:
-			self.red -= 1
+			self.red -= speed
 		if self.green_more_flag and self.green < 255:
-			self.green += 1
+			self.green += speed
 		if self.green_less_flag and self.green > 0:
-			self.green -= 1
+			self.green -= speed
 		if self.blue_more_flag and self.blue < 255:
-			self.blue += 1
+			self.blue += speed
 		if self.blue_less_flag and self.blue > 0:
-			self.blue -= 1
+			self.blue -= speed
 
 	def _check_events(self):
 		#Обрабатывает нажатия клавиш
@@ -68,6 +97,9 @@ class ColoredScreen():
 			self.blue_more_flag = True
 		elif event.key == pygame.K_d:
 			self.blue_less_flag = True
+		elif event.key == pygame.K_i:
+			self.i_flag = not self.i_flag
+
 
 	def _check_keyup_events(self, event):
 		#Реакция на отпускание клавиш
@@ -87,11 +119,13 @@ class ColoredScreen():
 	def _update_screen(self):
 		#Обновляет изображения на экране и отображает новый экран
 		self.screen.fill((self.red, self.green, self.blue))
+		if self.i_flag:
+			self.instruct.blitme()
 		pygame.display.flip()
 
 if __name__ == '__main__':
 	#создание экземпляра и запуск игры
-	Colors = ColoredScreen()
-	Colors.run_program()
+	colors = ColoredScreen()
+	colors.run_program()
 
 
